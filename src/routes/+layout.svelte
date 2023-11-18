25a2d0e501a53788137cd4c56da9c7afdc1d666c
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2023-11-18 11:59:49 +0530

    files setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,26 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
+	import Navbar from '../components/Navbar.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
+	import Explorer from '../components/Explorer.svelte';
+	import OpenFiles from '../components/OpenFiles.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
+<svelte:head>
+	<title>Avaneesh</title>
+	<meta name="description" content="Avaneesh's personal portfolio made using svelte-kit" />
+</svelte:head>
+
 <div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<Navbar />
+	<div class="layout">
+		<Sidebar />
+		<Explorer />
+		<div class="container">
+			<OpenFiles />
+			<slot />
+		</div>
+	</div>
+	<Footer />
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>

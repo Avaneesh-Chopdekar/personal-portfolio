469c982b76cd356f065756dfae0e7c2dae3e7f35
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2023-11-18 12:07:38 +0530

    added navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,81 @@
+<script>
+	import vscodeIcon from '$lib/images/vsc-icons/vscode_icon.svg';
+</script>
+
+<header>
+	<nav class="navbar">
+		<div class="left">
+			<img src={vscodeIcon} width="16" height="16" alt="VScode" />
+			<ul>
+				<li>File</li>
+				<li>Edit</li>
+				<li>View</li>
+				<li>Go</li>
+				<li>Run</li>
+				<li>Terminal</li>
+				<li>Help</li>
+			</ul>
+		</div>
+		<p class="middle">Avaneesh - Visual Studio Code</p>
+		<div class="right">
+			<span class="green"></span>
+			<span class="yellow"></span>
+			<span class="red"></span>
+		</div>
+	</nav>
+</header>
+
+<style>
+	.navbar {
+		border-bottom: 1px solid var(--dark);
+		width: 100%;
+		padding: 0.4rem;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.navbar div {
+		display: flex;
+		align-items: center;
+	}
+	.navbar ul {
+		display: flex;
+		align-items: center;
+		gap: 0.8rem;
+		margin-left: 0.75rem;
+		font-size: 0.8rem;
+	}
+	.navbar ul li {
+		cursor: pointer;
+	}
+	.middle {
+		z-index: -1;
+		user-select: none;
+		font-size: 0.8rem;
+		text-align: center;
+		position: absolute;
+		left: 0;
+		right: 0;
+	}
+	.right span {
+		height: 14px;
+		width: 14px;
+		border-radius: 50%;
+		margin: 0 0.25rem;
+		cursor: pointer;
+	}
+	.right .red {
+		background-color: #db4b4b;
+	}
+	.right .yellow {
+		background-color: khaki;
+	}
+	.right .green {
+		background-color: limegreen;
+	}
+	@media screen and (max-width: 800px) {
+		.navbar ul {
+			display: none;
+		}
+	}
+</style>

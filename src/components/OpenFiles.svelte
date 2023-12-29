3925a90a9b178924fc2c1b6243eeb79d38642da7
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2023-12-29 20:23:12 +0530

    fixed double side border on open files

--- src/components/OpenFiles.svelte
+++ src/components/OpenFiles.svelte
@@ -57,7 +57,7 @@
 		border-top: 1px solid transparent;
 		border-right: 1px solid var(--dark);
 	}
-	ul:first-child a {
+	li:first-child > a {
 		border-left: 1px solid var(--dark);
 	}
 	li > a:hover {
